4a25ab563179865bb529245832b6fa729ab414de
TranChung <tranchung927@gmail.com>
2017-07-14 07:57:38 +0700

    Place and current

--- GoogleMapsApp/GoogleMapsApp/MapsViewController.swift
+++ GoogleMapsApp/GoogleMapsApp/MapsViewController.swift
@@ -55,22 +55,90 @@ class MapsViewController: UIViewController {
         super.viewDidLoad()
         locationManager.delegate = self
         
+        placesClient = GMSPlacesClient.shared()
 
-        // Do any additional setup after loading the view.
-    }
-
-    func listLikePlaces() {
+        // Create a map.
+        let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude, longitude: defaultLocation.coordinate.longitude, zoom: zoomLevel)
+        mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+        mapView.settings.myLocationButton = true
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
+        // Add the map to the view, hide it until we&#39;ve got a location update.
+        view.addSubview(mapView)
+        mapView.isHidden = true
+        
+        listLikePlaces()
+    }
+    // Populate the array with the list of likely places.
+    func listLikePlaces() {
+        // Clean up from previous sessions.
+        likelyPlaces.removeAll()
+        
+        placesClient.currentPlace (callback: { (placeLikelihoods , error) -> Void in
+            if let error = error {
+                print("Current places error: \(error.localizedDescription)")
+                return
+            }
+            // Get likely places and add to the list.
+            if let likelihoodList = placeLikelihoods {
+                for likelihood in likelihoodList.likelihoods {
+                    let place = likelihood.place
+                    self.likelyPlaces.append(place)
+                }
+            }
+        })
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "segueToSelect" {
+            if let nextViewController = segue.destination as? PlacesViewController {
+                nextViewController.likelyPlaces = likelyPlaces
+            }
+        }
     }
-    */
-
+}
+// Delegates to handle events for the location manager.
+extension MapsViewController: CLLocationManagerDelegate {
+    
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location\(location)")
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+        
+        listLikePlaces()
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted")
+        case .denied:
+            print("User denied access to location")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK")
+        }
+    }
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error\(error)")
+    }
 }
--- GoogleMapsApp/GoogleMapsApp/PlacesViewController.swift
+++ GoogleMapsApp/GoogleMapsApp/PlacesViewController.swift
@@ -7,29 +7,66 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class PlacesViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+     // An array to hold the list of possible locations.
+    var likelyPlaces: [GMSPlace] = []
+    var selectedPlace: GMSPlace?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Register the table view cell class and its reuse id.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "unwindToMain" {
+            if let nextViewController = segue.destination as? MapsViewController {
+                nextViewController.selectedPlaces = selectedPlace
+            }
+        }
     }
-    */
-
+}
+// Respond when a user selects a place.
+extension PlacesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPlace = likelyPlaces[indexPath.row]
+        performSegue(withIdentifier: "unwindToMain", sender: self)
+    }
+    
+    // Adjust cell height to only show the first five items in the table
+    // (scrolling is disabled in IB).
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.tableView.frame.size.height/5
+    }
+    
+    // Make table rows display at proper height if there are less than 5 items.
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if (section == tableView.numberOfSections - 1) {
+            return 1
+        }
+        return 0
+    }
+}
+extension PlacesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return likelyPlaces.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let collectionItem = likelyPlaces[indexPath.row]
+        cell.textLabel?.text = collectionItem.name
+        return cell
+    }
 }
